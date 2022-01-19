478b421a7ad968ca5683f4105f30b7b030700977
Kevin Lacson <lacsonky@gmail.com>
2022-01-19 14:43:01 -0500

    started seed file

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const mysql = require("mysql2");
 const cTable = require("console.table");
 const { mainMenu } = require("./utils/prompts");
+const Employee = require("./lib/Employee");
 
 async function connectDb() {
     const connect = await mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "MySqlServer",
-        database: "employees",
+        database: "mycompany",
     });
     const db = connect.promise();
     let choice = await mainMenu();
-
+    if (choice) {
+        const test = new Employee("kevin", "lacson", "2", "3");
+        test.insert();
+    }
     const [employees] = await db.query("SELECT * FROM employee");
     const [departments] = await db.query("SELECT * FROM department");
     const [roles] = await db.query("SELECT * FROM role");
---
+++ utils/queries.js
@@ -0,0 +1,1 @@
+// SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee, role;
